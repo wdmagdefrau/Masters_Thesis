fd0507075b4e00264d0b8ae7f77b5b854469a96a
Derrick Magdefrau <w.d.magdefrau@gmail.com>
2018-10-11 01:37:24 -0700

    V2 - Validation accuracy vs run time added as an output plot. Optimum batch size occurs when the rate of performance change (change in accuracy per unit time) is maximized

--- run_v2.py
+++ run_v2.py
@@ -176,13 +176,6 @@ def show_results(results, year, print_results=[], plot_roc=False):
         plt.xlabel('False positive rate')
         plt.ylabel('True positive rate')
 
-    plt.figure()
-    plt.title('Accuracy vs Time')
-    #plt.plot((0, 1), (0, 1), ls='--', c='k')
-    plt.plot(results['run_time'], results['accuracy'])
-    plt.xlabel('Time (seconds)')
-    plt.ylabel('Accuracy (%)')
-
 if __name__ == '__main__':
     try:
         yaml_path = r"C:\Users\Derrick\Documents\GitHub\Masters_Thesis\configs\config_mlp.yml"
